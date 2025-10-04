6a1adc4d0eb866330f92c97de000ee96b88e1d95
KrishJain-web <krishjainb24bb1016@gmail.com>
2025-10-04 08:45:35 +0530

    Update tokenizer.py

--- tokenizer/tokenizer.py
+++ tokenizer/tokenizer.py
@@ -2,10 +2,7 @@
 import importlib
 import tiktoken
 tokenizer = tiktoken.get_encoding("gpt2")
-text = (
-    "Hello, do you like tea? <|endoftext|> In the sunlit terraces"
-     "of someunknownPlace."
-)
+text = input()
 integers = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
 print(integers)
 strings = tokenizer.decode(integers)
